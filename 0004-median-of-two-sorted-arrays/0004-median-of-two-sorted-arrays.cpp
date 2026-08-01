6e027f6c2b711eab9fd6854639ae2db78ec6b7b7
Atul Sanjay <atulsingh.xd@gmail.com>
2026-08-02 00:54:48 +0530

    Time: 0 ms (100%), Space: 95.6 MB (37.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,25 +3,21 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
          int m=nums1.size(), n=nums2.size();
 
-        vector<int> merged;
-        merged.reserve(m + n);
-
-        merged.insert(merged.end(),nums1.begin(), nums1.end());
-        merged.insert(merged.end(), nums2.begin(), nums2.end());
+        nums1.insert(nums1.end(),nums2.begin(), nums2.end());
         
-        sort(merged.begin(), merged.end());
+        sort(nums1.begin(), nums1.end());
 
-        int x=merged.size();
+        int x=nums1.size();
         double k=0;
         if((x%2)!=0){
             int y=(x/2);
-            double k=merged[y];
+            double k=nums1[y];
             return k;
 
         }
         else{
             float y=(x/2);
-            float z=merged[y]+merged[y-1];
+            float z=nums1[y]+nums1[y-1];
             double k=z/2;
             return k;
         }
