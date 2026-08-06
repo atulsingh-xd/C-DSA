a8e28628899ca13aa75dd5baeba8e7f7c9f50daf
Atul Sanjay <atulsingh.xd@gmail.com>
2026-08-07 02:27:06 +0530

    Time: 0 ms (100%), Space: 71.8 MB (15.18%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,22 @@
+const auto fastIO = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int i, n=nums.size(), maxSum=INT_MIN, currSum=0;
-        for(i=0;i<n;i++){
-            currSum+=nums[i];
-            maxSum=max(currSum, maxSum);
-            if(currSum<0){
-                currSum=0;
-            }
+        int maxSum = nums[0];
+        int currSum = nums[0];
+
+        const size_t n = nums.size();
+
+        for (size_t i = 1; i < n; ++i) {
+            currSum = max(nums[i], currSum + nums[i]);
+            maxSum = max(maxSum, currSum);
         }
         return maxSum;
-        
     }
 };
